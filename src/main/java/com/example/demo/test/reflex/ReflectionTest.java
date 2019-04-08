c2b9ff3706e531d9a91f258cec136a869bc14afa
yongqiang.zhu <981162250@qq.com>
2019-04-09 00:28:14 +0800

    反射添加日志

--- src/main/java/com/example/demo/test/reflex/ReflectionTest.java
+++ src/main/java/com/example/demo/test/reflex/ReflectionTest.java
@@ -5,6 +5,8 @@ import java.lang.reflect.Field;
 /**
  * 测试类
  * Java反射就是在运行状态中，对于任意一个类，都能够知道这个类的所有属性和方法；对于任意一个对象，都能够调用它的任意方法和属性；并且能改变它的属性。
+ *
+ * 结合加解密了解反射机制，待了解TODO
  */
 public class ReflectionTest {
 	public static void main(String[] args) throws Exception {
