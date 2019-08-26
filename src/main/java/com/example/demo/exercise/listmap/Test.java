73ad3bf1c8e75ed1daa09b469194d9405b196265
yongqiang.zhu <981162250@qq.com>
2019-08-26 23:23:36 +0800

    listmap合并成大listmap是有对个map组合而成

--- src/main/java/com/example/demo/exercise/listmap/Test.java
+++ src/main/java/com/example/demo/exercise/listmap/Test.java
@@ -14,13 +14,33 @@ public class Test {
 		List<Map<String,String>> mapList = new ArrayList<>();
 		Map<String,String> map = new HashMap<>();
 		map.put("proCode","proOne");
+		map.put("checkDate","2018-06-01");
 		map.put("count","12");
 		mapList.add(map);
 
 		Map<String,String> map1 = new HashMap<>();
-		map1.put("proCode","proTwo");
-		map1.put("count","12");
+		map1.put("proCode","proOne");
+		map.put("checkDate","2018-06-02");
+		map1.put("count","23");
 		mapList.add(map1);
+
+		List<Map<String,String>> mapList2 = new ArrayList<>();
+		Map<String,String> map3 = new HashMap<>();
+		map3.put("proCode","proTwo");
+		map3.put("checkDate","2018-06-01");
+		map3.put("count","15");
+		mapList2.add(map3);
+
+		Map<String,String> map4= new HashMap<>();
+		map4.put("proCode","proTwo");
+		map4.put("checkDate","2018-06-02");
+		map4.put("count","28");
+		mapList2.add(map4);
+
+		mapList.addAll(mapList2);
+
+
+
 
 
 		System.out.println(mapList);
