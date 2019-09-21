fd86aaada34927db6953e82568e8800f19abf0c0
yongqiang.zhu <981162250@qq.com>
2019-09-22 00:32:45 +0800

    sql提交

--- src/doc/sql_exercise.sql
+++ src/doc/sql_exercise.sql
@@ -2,3 +2,6 @@
 
 -- 在 SQL 中增加 HAVING 子句原因是，WHERE 关键字无法与合计函数一起使用
 SELECT sum(price) FROM `order` GROUP BY name HAVING sum(price) > 60;
+
+-- 添加银行卡名，在name字段之后
+ALTER TABLE `order` ADD COLUMN `bank`  varchar(50) NULL COMMENT '银行' AFTER `name`;
