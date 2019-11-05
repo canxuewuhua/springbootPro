e97bd1fd6e3dc2d7d74b2686a8517eb04b7b96bc
zhuyongqiang <zhuyongqiang@kingsoft.com>
2019-11-05 09:48:19 +0800

    四舍五入的金额

--- src/main/java/com/example/demo/exercise/amount/AmountTest.java
+++ src/main/java/com/example/demo/exercise/amount/AmountTest.java
@@ -7,7 +7,26 @@ import java.math.BigDecimal;
  * @date 2019/5/26 19:37
  */
 public class AmountTest {
+
+	public static final int SYSTEM_HALF_UP = BigDecimal.ROUND_HALF_UP;
 	public static void main(String[] args) {
+
+	}
+
+	/**
+	 *  四舍五入保留三位
+	 */
+	private void preserveDecimal(){
+		// 四舍五入保留三位
+		BigDecimal mistakeNum = new BigDecimal("200000");
+		BigDecimal mistakeRate = mistakeNum.divide(new BigDecimal("6"), 3, SYSTEM_HALF_UP);
+		System.out.println(mistakeRate);
+	}
+
+	/**
+	 * 将字符串翻转，需要将字符串转化为StringBuffer后
+	 */
+	private void reverseStr(){
 		int m = 101;
 		String s = String.valueOf(m);
 		StringBuffer sb = new StringBuffer(s);
@@ -17,6 +36,5 @@ public class AmountTest {
 		}else{
 			System.out.println("NO");
 		}
-
 	}
 }
