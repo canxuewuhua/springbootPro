2353c98d4f504704f1f7c3c515746c67e8c97289
zhuyongqiang <zhuyongqiang@kingsoft.com>
2019-11-30 18:38:36 +0800

    http请求参数拼接完善

--- src/main/java/com/example/demo/util/StringConcatUtil.java
+++ src/main/java/com/example/demo/util/StringConcatUtil.java
@@ -5,9 +5,13 @@ package com.example.demo.util;
  */
 public class StringConcatUtil {
 
-    public static final String httpUrl = "/html/activity/flow-register-d.html?vendor=%s&fromSource=%s&mobile=%s";
+    public static final String httpUrl = "/h5/activity/20190521YKD-YD/index.html?subChannel=%s&inChannel=%s&recommended=%s&token=%s";
 
     public static String concatHttpUrl(){
-        return String.format(httpUrl,  "bbb", "ccc", "17310353508");
+        return String.format(httpUrl,  "207", "207" + "jinshan","ykdjinshan","LD_8V3E5RSZ4VMQP9P38K8T5YYENSX3R683");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(concatHttpUrl());
     }
 }
